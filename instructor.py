095932ce1e365ce04fdde1e9bee344a089343334
Cherkesky <guy@cherkesky.com>
2020-01-16 11:32:13 -0600

    instructor class

--- instructor.py
+++ instructor.py
@@ -6,4 +6,17 @@
 # Slack handle
 # The instructor's cohort
 # The instructor's specialty (e.g. dad jokes, excitement, dancing, etc.)
+
 # A method to assign an exercise to a student
+
+
+class Instructor:
+  def __init__ (self, first, last, slack, specialty):
+    self.first = first
+    self.last = last
+    self.slack = slack
+    self.specialty  = specialty
+
+  def assign_to_cohort(exercise_id, student_id):
+    self.student_id.exercises.append(exercise_id)
+

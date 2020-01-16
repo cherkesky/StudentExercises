71e632340ffc6de096c30fe869a716ad3dbdb53b
Cherkesky <guy@cherkesky.com>
2020-01-16 11:18:01 -0600

    instructor doctype

---
+++ instructor.py
@@ -0,0 +1,9 @@
+# Instructor
+# You must define a type for representing an instructor in code.
+
+# First name
+# Last name
+# Slack handle
+# The instructor's cohort
+# The instructor's specialty (e.g. dad jokes, excitement, dancing, etc.)
+# A method to assign an exercise to a student

82f281f6defe6eba871deda2d58dee7d9f5288fa
Cherkesky <guy@cherkesky.com>
2020-01-16 11:17:28 -0600

    cohort doctype

---
+++ cohort.py
@@ -0,0 +1,6 @@
+# Cohort
+# You must define a type for representing a cohort in code.
+
+# The cohort's name (Evening Cohort 6, Day Cohort 26, etc.)
+# The collection of students in the cohort.
+# The collection of instructors in the cohort.

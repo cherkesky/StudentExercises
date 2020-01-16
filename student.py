04a821244ed493753e0009296a1e8e52380e94d6
Cherkesky <guy@cherkesky.com>
2020-01-16 11:16:45 -0600

    initial file structure

---
+++ student.py
@@ -0,0 +1,10 @@
+# Student
+# You must define a custom type for representing a student in code. A student can only be in one cohort at a time. A student can be working on many exercises at a time.
+
+# Properties
+# First name
+# Last name
+# Slack handle
+# The student's cohort
+# The collection of exercises that the student is currently working on
+

08fe63789e6f5105a478cdf4e89973e494d06f0d
Cherkesky <guy@cherkesky.com>
2020-01-16 11:18:41 -0600

    main doctype

---
+++ main.py
@@ -0,0 +1,7 @@
+# Exercise
+# You must define a type for representing an exercise in code. An exercise can be assigned to many students.
+
+# Name of exercise
+# Language of exercise (JavaScript, Python, CSharp, etc.)
+# Objective
+# The learning objective of this exercise is to practice creating instances of custom types that you defined with class, establishing the relationships between them, and practicing basic data structures in Python.

3bc91d1dbfdf03463d5e151f082f21bd419ae3b9
Cherkesky <guy@cherkesky.com>
2020-02-04 18:13:08 -0600

    added all single reports

--- exercise.py
+++ exercise.py
@@ -7,4 +7,6 @@ class Exercise:
   def __init__ (self, name, language):
     self.name = name
     self.language = language
+  def __repr__(self):
+      return f'{self.name} is an exercise in {self.language}'
 
--- reports.py
+++ reports.py
@@ -1,6 +1,7 @@
 import sqlite3
 from student import Student
 from cohort import Cohort
+from exercise import Exercise
 
 class StudentExerciseReports():
 
@@ -30,7 +31,7 @@ class StudentExerciseReports():
             """)
 
             all_students = db_cursor.fetchall() # list of tuples
-            print (all_students)
+           # print (all_students)
 
             # for student in all_students:
             #   print(f'{student[1]} {student[2]} is in {student[5]}')
@@ -57,17 +58,81 @@ class CohortsReport:
 
        """)
       all_cohorts = db_cursor.fetchall()
-      print(all_cohorts)
+      #print(all_cohorts)
       for cohort in all_cohorts:
         print(cohort)
 
 ########################## ALL EXERCISES ##########################
+class ExercisesReport:
+  def __init__(self):
+    self.db_path = "/Users/cherkesky/workspace/python/StudentExercises/studentexercises.db"
+
+  def all_exercises(self):
+    with sqlite3.connect(self.db_path) as conn:
+      conn.row_factory = lambda cursor, row: Exercise (row[1], row[2])
+      db_cursor = conn.cursor()
+      db_cursor.execute("""
+      
+      select * from Exercise
+
+       """)
+      all_exercises = db_cursor.fetchall()
+      #print(all_exercises)
+      for exercise in all_exercises:
+        print(exercise)
+########################## JAVASCRIPT EXERCISES ##########################
+class JSExercisesReport:
+  def __init__(self):
+    self.db_path = "/Users/cherkesky/workspace/python/StudentExercises/studentexercises.db"
+
+  def js_exercises(self):
+    with sqlite3.connect(self.db_path) as conn:
+      conn.row_factory = lambda cursor, row: Exercise (row[1], row[2])
+      db_cursor = conn.cursor()
+      db_cursor.execute("""
+      
+      select * from Exercise
+      where Exercise_Language = 'JavaScript';
+
+       """)
+      js_exercises = db_cursor.fetchall()
+      #print(js_exercises)
+      for exercise in js_exercises:
+        print(exercise)
+########################## PYTHON EXERCISES ##########################
+class PYExercisesReport:
+  def __init__(self):
+    self.db_path = "/Users/cherkesky/workspace/python/StudentExercises/studentexercises.db"
+
+  def py_exercises(self):
+    with sqlite3.connect(self.db_path) as conn:
+      conn.row_factory = lambda cursor, row: Exercise (row[1], row[2])
+      db_cursor = conn.cursor()
+      db_cursor.execute("""
+      
+      select * from Exercise
+      where Exercise_Language = 'Python';
+
+       """)
+      py_exercises = db_cursor.fetchall()
+      #print(py_exercises)
+      for exercise in py_exercises:
+        print(exercise)
 
 
 
-reports = StudentExerciseReports()
 print ("################### ALL STUDENTS REPORT ################### ")
+reports = StudentExerciseReports()
 reports.all_students()
-cohorts = CohortsReport()
 print ("################### COHORTS REPORT ################### ")
+cohorts = CohortsReport()
 cohorts.all_cohorts()
+print ("################### EXERCISES REPORT ################### ")
+exercises = ExercisesReport()
+exercises.all_exercises()
+print ("################### JAVASCRIPT REPORT ################### ")
+jsexercises = JSExercisesReport()
+jsexercises.js_exercises()
+print ("################### PYTHON REPORT ################### ")
+pyexercises = PYExercisesReport()
+pyexercises.py_exercises()
